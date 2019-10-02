d290fe3a07628d5a89603e92ec8bf67f2eecc197
heineaabo <heine.aabo@gmail.com>
2019-10-02 18:59:52 +0200

    added doc

---
+++ doc/operator.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Operator:
+    def __init__(self,operation):
+        self.op = operation
+
+    def __add__(self,op2):
+        return None
+
+    def __mul__(self,op2):
+        return None
+
+    def __sub__(self,op2):
+        return None
+
+    def __inv__(self):
+        return None
