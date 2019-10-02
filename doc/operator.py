44ab2b2352e33b004e9ef9da860c1130dee81cfe
Heine Olsson Aabø <heine2307@MacBook-Pro-2.local>
2019-10-02 23:12:27 +0200

    update

--- doc/operator.py
+++ doc/operator.py
@@ -1,17 +1,41 @@
 import numpy as np
 
 class Operator:
-    def __init__(self,operation):
-        self.op = operation
-
-    def __add__(self,op2):
-        return None
+    def __init__(self,operation=''):
+        self.op = operation.upper()
+        print(self.op)
 
     def __mul__(self,op2):
-        return None
+        return self.op + op2.op
 
-    def __sub__(self,op2):
-        return None
 
+class Pauli(Operator):
     def __inv__(self):
-        return None
+        return self.op
+
+class Identity(Operator):
+    def __mul__(self,op2):
+        return op2.op
+    
+class Ladder(Operator):
+    def __init__(self,operation):
+        self.op = operation
+        self.values = ['+','-']
+        self.ind = self.values.index(self.op)
+    
+    def __invert__(self): 
+        self.ind = (self.ind + 1)%2
+        self.op = self.values[self.ind]
+        return self.op
+
+
+pauliX = Operator('x')
+pauliY = Operator('y')
+pauliZ = Pauli('Z')
+I = Identity('')
+plus = Ladder('+')
+print(~Ladder)
+
+print(pauliX * pauliY)
+print(pauliX * pauliZ)
+print(pauliX * I)
