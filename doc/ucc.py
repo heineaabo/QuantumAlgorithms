6bffe267202ac9ede0a93b70741962c9d31858f9
heineaabo <heine.aabo@gmail.com>
2020-01-29 17:40:01 +0100

    rewritten to class

--- doc/ucc.py
+++ doc/ucc.py
@@ -1,61 +1,151 @@
 import numpy as np
 import qiskit as qk
 
-def UCCS(system,qc,qb,theta,depth=10):
-    n_qubits = system[0]
-    n_fermi = system[1]
-    theta = theta.reshape(n_qubits,n_qubits-n_fermi)
+class UnitaryCoupledCluster:
+    """
+    Unitary Coupled Cluster ansatz, with optimized singles and doubles circuits.
+    Assumes the convention:
+        ❘0⟩ - occupied orbital.
+        ❘1⟩ - unoccupied orbital.
 
-    # Prepare reference state
-    for k in range(n_fermi,n_qubits):
-        qc.x(qb[k])
+    Initialize class input parameters below.
+    Initialize state to Hartree-Fock state with prepare_Hartree_state(circuit,qregister)
+    Call class to perform UCC.
 
-    # UCCS
-    for rho in range(depth):
+    Input:
+            n (int): Number of particles.    
+            l (int): Number of spin orbitals.
+        trunc (str): CC truncation.
+                     - 'S' for singles.
+                     - 'D' for doubles.
+                     - 'SD' for singles and doubles.
+        depth (int): Depth of Trotter approximation. 
+    """
+
+    def __init__(self,n,l,trunc,depth):
+        self.n = n
+        self.l = l
+        self.trunc = trunc
+        self.depth = depth
+        self.num_S, self.num_D = self.get_num_parameters()
+        if 'S' not in self.trunc:
+            self.num_S = 0
+        if 'D' not in self.trunc:
+            self.num_D = 0
+
+    def __call__(self,qc,qb,theta):
+        theta = self.sort_theta(theta)
+        for rho in range(self.depth):
+            if 'S' in self.trunc:
+                qc = self.UCCS(qc,qb,theta[0],rho)
+            if 'D' in self.trunc:
+                qc = self.UCCD(qc,qb,theta[1],rho)
+        if 'S' not in self.trunc and 'D' not in self.trunc:
+            raise ValueError('Need to spesify correct UCC type')
+        return qc
+
+    def get_num_parameters(self):
+        S = self.n*(self.l-self.n)
+        D = 0
+        for i in range(self.n):
+            for j in range(i+1,self.n):
+                for a in range(self.n,self.l):
+                    for b in range(a+1,self.l):
+                        D += 1
+        return S,D
+
+    def new_parameters(self):
+        return 2*np.pi*np.random.randn((self.num_S+self.num_D)*self.depth)
+
+
+    def prepare_Hartree_state(self,qc,qb):
+        """
+        Prepares Hartree-Fock state. Assume initialized to ❘0...0⟩ before 
+        Example for n=2 and l=4 -> ❘0011⟩
+        """
+        # Reference state
+        for k in range(self.n):
+            qc.x(qb[k])
+        return qc
+
+    def sort_theta(self,theta):
+        """
+        Sort parameters to two channels for singles and doubles.
+        TODO: 
+            - Sort for depth > 1.
+        """
+        singles = self.num_S
+        doubles = self.num_D
+        assert len(theta) == (singles+doubles)*self.depth
+        #new_theta = [theta[:singles],theta[singles:]]
+        thetaS = []
+        thetaD = []
+        for i in range(self.depth):
+            thetaS.append(theta[(i*singles):(i+1)*singles])
+            thetaD.append(theta[(self.depth*singles + (i)*doubles):(self.depth*singles + (i+1)*doubles)])
+        #new_theta = [theta[:singles],theta[singles:]]
+        new_theta = [thetaS,thetaD]
+        return new_theta
+
+    def UCCS(self,qc,qb,theta,rho):
+        """
+        Optimized UCC singles implementation.
+        """
+        n_qubits = self.l
+        n_fermi = self.n
+        #theta = theta.reshape(n_fermi,n_qubits-n_fermi)
+
+        t = 0
+        # UCCS
         for i in range(n_fermi):
             for a in range(n_fermi,n_qubits):
                 # Pauli-z
                 for k in range(i+1,a):
                     qc.z(qb[k])
-                
                 # Pauli strings
                 qc.h(qb[i])
                 qc.rx(np.pi/2,qb[i])
                 qc.h(qb[a])
 
                 qc.cx(qb[i],qb[a])
-                qc.rz(theta[i,a-n_fermi]/2,qb[a])
+
+                #qc.rz(theta[i,a-n_fermi]/2,qb[a])
+                qc.rz(theta[rho][t]/2,qb[a])
+
                 qc.cx(qb[i],qb[a])
 
                 qc.rx(-np.pi/2,qb[i])
                 qc.rx(np.pi/2,qb[a])
 
                 qc.cx(qb[i],qb[a])
-                qc.rz(-theta[i,a-n_fermi]/2,qb[a])
+
+                #qc.rz(-theta[i,a-n_fermi]/2,qb[a])
+                qc.rz(-theta[rho][t]/2,qb[a])
+
                 qc.cx(qb[i],qb[a])
 
                 qc.h(qb[i])
                 qc.rx(-np.pi/2,qb[a])
                 qc.h(qb[a])
 
-    return qc
+                t += 1
 
-def UCCD(system,qc,qb,theta,depth=1):
-    n_qubits = system[0]
-    n_fermi = system[1]
-    theta = theta.reshape(n_fermi,n_fermi,n_qubits-n_fermi,n_qubits-n_fermi)
+        return qc
 
-    # Prepare reference state
-    for k in range(n_fermi,n_qubits):
-        qc.x(qb[k])
+    def UCCD(self,qc,qb,theta,rho=0):
+        """
+        Optimized UCC doubles implementation.
+        """
+        n_qubits = self.l
+        n_fermi = self.n
+        #theta = theta.reshape(n_fermi,n_fermi,n_qubits-n_fermi,n_qubits-n_fermi)
 
-    # UCCD
-    for rho in range(depth):
+        t = 0
+        # UCCD
         for i in range(n_fermi):
             for j in range(i+1,n_fermi):
                 for a in range(n_fermi,n_qubits):
                     for b in range(a+1,n_qubits):
-
                         # Pauli-z
                         for k in range(i+1,j):
                             qc.z(qb[k])
@@ -73,7 +163,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[a],qb[b])
 
-                        qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xxyx)
+                        #qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xxyx)
+                        qc.rz(theta[rho][t]/8,qb[b]) #(xxyx)
                         
                         qc.cx(qb[a],qb[b])
                         qc.cx(qb[j],qb[b])
@@ -84,7 +175,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[a],qb[b])
 
-                        qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xyxx)
+                        #qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xyxx)
+                        qc.rz(-theta[rho][t]/8,qb[b]) #(xyxx)
                         
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[i],qb[b])
@@ -95,7 +187,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[i],qb[b])
                         qc.cx(qb[j],qb[b])
 
-                        qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yxxx)
+                        #qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yxxx)
+                        qc.rz(-theta[rho][t]/8,qb[b]) #(yxxx)
 
                         qc.cx(qb[a],qb[b])
                         qc.cx(qb[j],qb[b])
@@ -106,7 +199,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[a],qb[b])
 
-                        qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yyyx)
+                        #qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yyyx)
+                        qc.rz(-theta[rho][t]/8,qb[b]) #(yyyx)
                         
                         qc.cx(qb[a],qb[b])
                         qc.cx(qb[j],qb[b])
@@ -119,7 +213,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[a],qb[b])
 
-                        qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xyyy)
+                        #qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xyyy)
+                        qc.rz(theta[rho][t]/8,qb[b]) #(xyyy)
 
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[i],qb[b])
@@ -130,7 +225,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[i],qb[b])
                         qc.cx(qb[j],qb[b])
 
-                        qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yxyy)
+                        #qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yxyy)
+                        qc.rz(theta[rho][t]/8,qb[b]) #(yxyy)
 
                         qc.cx(qb[a],qb[b])
                         qc.cx(qb[j],qb[b])
@@ -141,7 +237,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[a],qb[b])
 
-                        qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yyxy)
+                        #qc.rz(-theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(yyxy)
+                        qc.rz(-theta[rho][t]/8,qb[b]) #(yyxy)
 
                         qc.cx(qb[j],qb[b])
                         qc.cx(qb[i],qb[b])
@@ -152,7 +249,8 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.cx(qb[i],qb[b])
                         qc.cx(qb[j],qb[b])
 
-                        qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xxxy)
+                        #qc.rz(theta[i,j,a-n_fermi,b-n_fermi]/8,qb[b]) #(xxxy)
+                        qc.rz(theta[rho][t]/8,qb[b]) #(xxxy)
 
                         qc.cx(qb[a],qb[b])
                         qc.cx(qb[j],qb[b])
@@ -163,25 +261,7 @@ def UCCD(system,qc,qb,theta,depth=1):
                         qc.h(qb[a])
                         qc.rx(-np.pi/2,qb[b])
                         qc.h(qb[b])
+                        
+                        t += 1
 
-    return qc
-
-def sort_theta(theta):
-    """
-    Return array with two elements corresponding to singles and doubles 
-    parameters respectively.
-    """
-    return theta
-
-def UCC(system,qc,qb,theta,terms='SD',depth=1):
-    if terms == 'S':
-        qc = UCCS(system,qc,qb,theta,depth=depth)
-    elif terms == 'D':
-        qc = UCCD(system,qc,qb,theta,depth=depth)
-    elif terms == 'SD':
-        theta = sort_theta(theta)
-        qc = UCCS(system,qc,qb,theta[0],depth=depth)
-        qc = UCCD(system,qc,qb,theta[1],depth=depth)
-    else:
-        raise ValueError('Need to spesify correct UCC type')
-    return qc
+        return qc
