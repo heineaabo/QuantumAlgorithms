44bc5cf8257960d03df89b9a4466d4a43d85832c
heineaabo <heine.aabo@gmail.com>
2020-06-17 12:07:15 +0200

    final2

--- quantum_algorithms/algorithm.py
+++ quantum_algorithms/algorithm.py
@@ -30,32 +30,46 @@ class QuantumAlgorithm:
         self.prnt = options.get('print')
         self.ancilla_measure = options.get('ancilla') if options.get('ancilla') != None else False
 
-        # For Backend
-        if options.get('backend') == None:
-            self.options['backend'] = 'qasm_simulator' 
-        self.backend = qk.Aer.get_backend(options['backend'])
-        # For noise model, coupling map and basis gates
-        self.noise_model, self.coupling_map, self.basis_gates = None,None,None
-        self.meas_fitter = None
-        if options.get('device') != None:
-            device = QuantumComputer(options.get('device'))
-            if options.get('noise_model') != None:
-                self.noise_model = device.noise_model
-                # Create error mitigation fitter
-                if options.get('meas_fit') in [None,True]:
-                    from attributes import get_measurement_fitter
-                    self.meas_fitter = get_measurement_fitter(l,
-                                                             self.backend,
-                                                             device,
-                                                             self.shots)
-            if options.get('coupling_map') != None:
-                self.coupling_map = device.coupling_map
-            if options.get('basis_gates') != None:
-                self.basis_gates = device.basis_gates
-        # Qubit layout, virtual to physical
+        self.ibmq = False
+        if options.get('ibmq') == True:
+            print('Running on real quantum computer')
+            self.ibmq = True
+            self.backend = options['backend']
+            from qiskit.tools.monitor import job_monitor
+            self.monitor = job_monitor
+            from attributes import get_measurement_fitter
+            self.meas_fitter = get_measurement_fitter(l,
+                                                     self.backend,
+                                                     None,
+                                                     self.shots)
+            
+        else:
+            # For Backend
+            if options.get('backend') == None:
+                self.options['backend'] = 'qasm_simulator' 
+            self.backend = qk.Aer.get_backend(options['backend'])
+            # For noise model, coupling map and basis gates
+            self.noise_model, self.coupling_map, self.basis_gates = None,None,None
+            self.meas_fitter = None
+            if options.get('device') != None:
+                device = QuantumComputer(options.get('device'))
+                if options.get('noise_model') != None:
+                    self.noise_model = device.noise_model
+                    # Create error mitigation fitter
+                    if options.get('meas_fit') in [None,True]:
+                        from attributes import get_measurement_fitter
+                        self.meas_fitter = get_measurement_fitter(l,
+                                                                 self.backend,
+                                                                 device,
+                                                                 self.shots)
+                if options.get('coupling_map') != None:
+                    self.coupling_map = device.coupling_map
+                if options.get('basis_gates') != None:
+                    self.basis_gates = device.basis_gates
+            # Qubit layout, virtual to physical
         self.layout = options.get('layout')
         # Optimization level
-        self.optimization_level=options.get('optimization_level')
+        self.optimization_level= 1 if options.get('optimization_level')==None else options['optimization_level']
 
         # GPU accelerated
         if options.get('gpu'):
@@ -74,14 +88,22 @@ class QuantumAlgorithm:
                 qc.measure(q,c)
         else:
             qc.measure(qb,cb)
-        job = qk.execute(qc, 
-                        backend = self.backend, 
-                        shots=self.shots,
-                        optimization_level=self.optimization_level,
-                        noise_model=self.noise_model,
-                        coupling_map=self.coupling_map,
-                        basis_gates=self.basis_gates,
-                        initial_layout=self.layout,
-                        seed_transpiler=self.seed,
-                        seed_simulator=self.seed)
+        if self.ibmq:
+            job = qk.execute(qc, 
+                            backend = self.backend, 
+                            shots=self.shots,
+                            optimization_level=self.optimization_level,
+                            initial_layout=self.layout)
+            self.monitor(job)
+        else:
+            job = qk.execute(qc, 
+                            backend = self.backend, 
+                            shots=self.shots,
+                            optimization_level=self.optimization_level,
+                            noise_model=self.noise_model,
+                            coupling_map=self.coupling_map,
+                            basis_gates=self.basis_gates,
+                            initial_layout=self.layout,
+                            seed_transpiler=self.seed,
+                            seed_simulator=self.seed)
         return job.result().get_counts(qc)
--- quantum_algorithms/ansatz/__init__.py
+++ quantum_algorithms/ansatz/__init__.py
@@ -1,3 +1,5 @@
 from .ucc import UnitaryCoupledCluster
-from .ryrz import RYRZ,RYpairing
-from .ry import RY
+from .ibm_ucc import UCC5
+from .ryrz import RYRZ
+from .ry import RY,RYpairing
+from .AND_gates import *
