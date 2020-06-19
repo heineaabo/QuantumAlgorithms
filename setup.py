c06f0406e44dd8e2ffa075abf9040b66256fb8f3
Heine Olsson Aabø <42807355+heineaabo@users.noreply.github.com>
2020-06-19 12:51:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="Quantum Algorithms", 
+        version="0.1", 
+        author ="Heine Olsson Aabø",
+        description="Quantum algorithms for finding the ground state of many-particle systems",
+        url="https://https://github.com/heineaabo/QuantumAlgorithms",
+        packages=find_packages()
+        )
+
+import sys
+sys.path.append('.')
